a6d4647e7cc93ff05d61f7efbab66907ed0b6f01
Michael Skinnider <michaelskinnider@gmail.com>
2017-10-31 11:04:54 -0700

    Create PrInCE R package: currently just implements Gaussian fitting in R

---
+++ R/fit_curve.R
@@ -0,0 +1,28 @@
+#' Output the fit curve for a given mixture of Gaussians
+#' 
+#' For a Gaussian mixture model fit to a curve by \code{\link{fit_gaussians}},
+#' output the fit curve using the coefficients rather than the \code{nls} 
+#' object. This allows individual Gaussians to be removed from the fit model:
+#' for example, if their height is below a certain threshold, or their 
+#' centres are outside the bounds of the chromatogram. 
+#' 
+#' @param coef numeric vector of coefficients for a Gaussian mixture model fit 
+#' by \code{\link{fit_gaussians}}. This function assumes that the heights of the 
+#' Gaussians are specified by coefficients beginning with "A" 
+#' ("A1", "A2", "A3", etc.), centres are specified by coefficients beginning
+#' with "mu", and standard deviations are specified by coefficients beginning
+#' with "sigma".  
+#' @param indices the indices, or x-values, to predict a fitted curve for 
+#' (for example, the fractions in a given chromatogram)
+#' 
+#' @return the fitted curve
+#' 
+#' @export
+fit_curve <- function(coef, indices) {
+  gaussians <- length(coef) / 3
+  A <- coef[startsWith(names(coef), "A")]
+  mu <- coef[startsWith(names(coef), "mu")]
+  sigma <- coef[startsWith(names(coef), "sigma")]
+  rowSums(sapply(seq_len(gaussians), function(i) 
+    A[i] * exp(-((indices - mu[i])/sigma[i])^2)))
+}
---
+++ R/make_initial_conditions.R
@@ -0,0 +1,72 @@
+#' Make initial conditions for curve fitting with a mixture of Gaussians
+#' 
+#' Construct a set of initial conditions for curve fitting using nonlinear
+#' least squares using a mixture of Gaussians. The "guess" method ports 
+#' code from the Matlab release of PrInCE. This method finds local maxima 
+#' within the chromatogram, orders them by their separation (in number of 
+#' fractions) from the previous local maxima, and uses the positions and heights
+#' of these local maxima (+/- some random noise) as initial conditions for 
+#' Gaussian curve-fitting. The "random" method simply picks random values 
+#' within the fraction and intensity intervals as starting points for Gaussian
+#' curve-fitting. The initial value of sigma is set by default to a random 
+#' number within +/- 0.5 of two for both modes; this is based on our manual 
+#' inspection of a large number of chromatograms. 
+#' 
+#' @param chromatogram a numeric vector corresponding to the chromatogram trace
+#' @param n_gaussians the number of Gaussians being fit 
+#' @param method one of "guess" or "random", discussed above
+#' @param seed the seed to use for random number generation
+#' @param sigma_default the default mean initial value of sigma
+#' @param sigma_noise the amount of random noise to add or subtract from 
+#' the default mean initial value of sigma
+#' @param mu_noise the amount of random noise to add or subtract from the 
+#' Gaussian centers in "guess" mode
+#' @param A_noise the amount of random noise to add or subtract from the 
+#' Gaussian heights in "guess" mode 
+#' 
+#' @return a list of three numeric vectors (A, mu, and sigma), each having
+#' a length equal to the maximum number of Gaussians to fit 
+#' 
+#' @export
+make_initial_conditions <- function(chromatogram, n_gaussians, 
+                                    method = c("guess", "random"),
+                                    seed = 1, sigma_default = 2, 
+                                    sigma_noise = 0.5, mu_noise = 1.5, 
+                                    A_noise = 0.5) {
+  # set seed
+  set.seed(seed)
+  method <- match.arg(method)
+  if (method == "guess") {
+    # find fractions that represent local maxima
+    peaksX <- which(diff(sign(diff(chromatogram))) == -2) + 1
+    # catch local minima at start or end
+    if (dplyr::first(chromatogram)[1] > dplyr::nth(chromatogram, 2))
+      peaksX <- c(peaksX, 1)
+    if (dplyr::last(chromatogram) > dplyr::nth(chromatogram, -2))
+      peaksX <- c(peaksX, length(chromatogram))
+    # order local maxima by distance (on one side, only)
+    distances <- diff(peaksX)
+    peaksX <- peaksX[order(-distances)]
+    # get intensities for sorted local maxima
+    peaksY <- chromatogram[peaksX]
+    # use local maxima as initial conditions for Gaussian fitting
+    A <- numeric(0)
+    mu <- numeric(0)
+    sigma <- numeric(0)
+    for (i in seq_len(n_gaussians)) {
+      A[i] <- peaksY[i] + runif(1) - 0.5
+      mu[i] <- peaksX[i] + runif(1, max = 3) - 1.5
+      sigma[i] <- sigma_default + runif(1) - 0.5
+    }
+    initial_conditions <- list(A = A, mu = mu, sigma = sigma)
+    return(initial_conditions)
+  } else if (method == "random") {
+    minHeight <- min(chromatogram)
+    maxHeight <- max(chromatogram)
+    A <- runif(n_gaussians, min = minHeight, max = maxHeight)
+    mu <- runif(n_gaussians, min = 1, max = length(chromatogram))
+    sigma <- sigma_default + runif(n_gaussians) - 0.5
+    initial_conditions <- list(A = A, mu = mu, sigma = sigma)
+    return(initial_conditions)
+  }
+}
