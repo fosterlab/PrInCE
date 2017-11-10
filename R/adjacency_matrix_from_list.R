f03d7e13ac0ba0fd1ba342fb8eddd73fea49b518
Michael Skinnider <michaelskinnider@gmail.com>
2017-11-10 14:29:53 -0800

    Convert a list of complexes to an adjacency matrix

---
+++ R/adjacency_matrix_from_list.R
@@ -0,0 +1,24 @@
+#' Create an adjacency matrix from a list of complexes
+#' 
+#' Convert a list of complexes into a pairwise adjacency matrix. The resulting
+#' square adjacency matrix contains ones for proteins that are found in the
+#' same complex and zeroes otherwise.
+#' 
+#' @param complexes a list of complexes, with each entry containing complex
+#' subunits as a character vector
+#' 
+#' @return an adjacency matrix between all complex subunits
+#' 
+#' @export
+adjacency_matrix_from_list <- function(complexes) {
+  proteins <- unique(unlist(complexes))
+  n_proteins <- length(proteins)
+  adjacency <- matrix(0, nrow = n_proteins, ncol = n_proteins,
+                      dimnames = list(proteins, proteins))
+  
+  # identify pairwise interactions
+  pairs <- map_dfr(complexes, ~ crossing(., .))
+  adjacency[as.matrix(pairs)] <- 1
+  
+  return(adjacency)
+}
