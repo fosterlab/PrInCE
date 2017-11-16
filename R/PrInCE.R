a0dc693eb704f2f783b0f9c8938d096fd226d8f3
Michael Skinnider <michaelskinnider@gmail.com>
2017-11-16 11:34:08 -0800

    Add references to main PrInCE function

--- R/PrInCE.R
+++ R/PrInCE.R
@@ -1,7 +1,22 @@
-#' Predicting Interactomes from Co-Elution
+#' PrInCE: Prediction of Interactomes from Co-Elution
 #' 
-#' <description>
+#' PrInCE is a pipeline 
 #' 
+#' @param profile_matrix a numeric matrix of co-elution profiles, or something
+#' that can be coerced to it, with proteins in rows
+#' @param gold_standard an adjacency matrix of gold-standard interactions.
+#' Zeroes in the adjacency matrix are interpreted by PrInCE as "true negatives"
+#' when calculating precision. 
+#' 
+#' @return a ranked data frame of interacting proteins, with the precision
+#' at each point in the list
+#' 
+#' @references 
+#' \insertRef{stacey2017}{PrInCE}
+#' 
+#' \insertRef{scott2015}{PrInCE}
+#' 
+#' \insertRef{kristensen2012}{PrInCE}
 PrInCE <- function(profile_matrix, gold_standard) {
   # check input 
   profile_matrix <- data.matrix(profile_matrix)
