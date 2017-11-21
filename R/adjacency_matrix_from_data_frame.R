2cfde81c3d82401f4053dbeaee97f83803e2b32c
Michael Skinnider <michaelskinnider@gmail.com>
2017-11-21 14:03:14 -0800

    Optionally make symmetric adjacency matrix from interaction data frame

--- R/adjacency_matrix_from_data_frame.R
+++ R/adjacency_matrix_from_data_frame.R
@@ -5,11 +5,13 @@
 #' proteins that are found in interactions and zeroes otherwise.
 #' 
 #' @param dat a data frame with pairwise interactions in the first two columns
+#' @param symmetric if true, interactions in both directions will be 
+#' added to the adjacency matrix 
 #' 
 #' @return an adjacency matrix between all interacting proteins
 #' 
 #' @export
-adjacency_matrix_from_data_frame <- function(dat) {
+adjacency_matrix_from_data_frame <- function(dat, symmetric = T) {
   proteins <- unique(c(dat[[1]], dat[[2]]))
   n_proteins <- length(proteins)
   adjacency <- matrix(0, nrow = n_proteins, ncol = n_proteins,
@@ -17,6 +19,9 @@ adjacency_matrix_from_data_frame <- function(dat) {
   
   # identify pairwise interactions
   adjacency[as.matrix(dat[, 1:2])] <- 1
-
+  # add symmetric interactions
+  if (symmetric) {
+    adjacency[as.matrix(dat[, 2:1])] <- 1
+  }
   return(adjacency)
 }
