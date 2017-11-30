2843d6458811f875e84e1b4f8c14c8b58c99b387
Michael Skinnider <michaelskinnider@gmail.com>
2017-11-30 09:11:06 -0800

    Replace missing data for RF/SVM classifiers

---
+++ R/replace_missing_data.R
@@ -0,0 +1,29 @@
+#' Replace missing data with mean ± random noise 
+#' 
+#' Replace missing data within each numeric column of a data frame with 
+#' the column mean, plus or minus some random noise, in order to train 
+#' classifiers that do not easily ignore missing data (e.g. random forests or
+#' support vector machines).
+#' 
+#' @param input the data frame to 
+#' @param noise_pct the standard deviation of the random normal 
+#' distribution from which to draw added noise, expressed as a 
+#' percentage of the standard deviation of the non-missing values in each 
+#' column
+#' 
+#' @return a data frame with missing values in each numeric column replaced
+#' by the column mean, plus or minus some random noise
+#' 
+#' @export
+replace_missing_data <- function(input, noise_pct = 0.05) {
+  for (col_name in colnames(input)) {
+    column <- input[[col_name]]
+    if (!is.numeric(column))
+      next
+    missing <- is.na(column)
+    input[[col_name]][missing] <- 
+      mean(column, na.rm = T) + rnorm(sum(missing)) * 
+      sd(column, na.rm = T) * noise_pct 
+  }
+  return(input)
+}
